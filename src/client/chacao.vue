e0657fdf688db9330137488430124f64b77c8d8c
zimogod <18518517713@163.com>
2020-08-05 15:31:42 +0800

    ff

---
+++ src/client/chacao.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="slots">
+    我是子组件
+    <!-- 匿名插槽 -->
+    <slot>我是默认数据</slot>
+  </div>
+</template>
+<script>
+export default {
+    name:'slots',
+    data(){
+        return {
+           
+        }
+    }, 
+}
+</script>
+<style lang="less">
+.slots{
+    width:100%;
+    height: 100%;
+}
+</style>
+
--- src/client/page.vue
+++ src/client/page.vue
@@ -1,29 +1,64 @@
 <template>
   <div class="page">
+      <!-- v-html -->
+      <!-- <div v-html="userInfo1"></div> -->
+      <!-- -------------------------------------- -->
+      <!-- v-text -->
+      <!-- <div v-text="userInfo"></div> -->
+      <!-- v-show -->
+      <!-- -------------------------------------- -->
+      <!-- <div v-show="!true">{{ userInfo }}---</div> -->
+      <!-- v-else-if --> 
+      <!-- <div v-if="!str">{{ str }}===</div> -->
+      <!-- <div v-else-if="userInfo">{{ userInfo }}222</div>
+      <div v-else>{{ info }}333</div> -->
+      <!-- v-if -->
+      <!-- <div v-if="info >= 21">{{ info }}</div>
+      <div v-else>{{ userInfo }}</div> -->
+     <!-- v-pre -->
+     <!-- <div v-pre>
+         <div>1234</div>
+         <div>zimo</div>
+         <div>{{ info }}</div>
+     </div> -->
       <!-- 声明式的写法 -->
-      <router-link to="/">home</router-link>
-      <div>{{ message }}</div>
+      <!-- <router-link to="/">home</router-link>
+      <div>{{ message }}</div> -->
       <!-- v-model -->
-      <input placeholder="请输入用户名" v-model="message" @change="onchanges">
+      <!-- <input placeholder="请输入用户名" v-model="message" @change="onchanges">
       <ul>
           <li v-for="(item,index) in arr" v-bind:key="index">{{ item }}---{{ index }}</li>
-      </ul>
+      </ul> -->
       <!-- v-for -->
-      <div v-for="(item,index) in obj" :key="index">
+      <!-- <div v-for="(item,index) in obj" :key="index">
           {{ item.name }}:{{ item.age }}
-      </div>
+      </div> -->
       <!-- v-on -->
-     <div v-on:click="handleClick">事件</div>
+     <!-- <div v-on:click="handleClick">事件</div> -->
      <!-- v-bind -->
-     <img v-bind:src="img" >
-     <img :src="img" >
+     <!-- <img v-bind:src="img" >
+     <img :src="img" > -->
+     <div>我是父组件</div>
+    <chacao>
+        <div>你是默认的也没用，我是V8，显示me</div>
+    </chacao>
   </div>
 </template>
 <script>
+import chacao from './chacao';
 export default {
     name:'page',
+    // 注册一个组件，使其合法化
+    components:{
+        chacao
+    },
     data(){
         return {
+            userInfo1:`<div>千秋万代</div>`,
+            isShow:'',
+            str:'reset',
+            userInfo:204,
+            info:18,
             img:require('../assets/logo.png'),
             message:'',
             arr:['神仙','地狱','凡人','神仙','地狱','凡人','神仙','地狱','凡人','神仙','地狱','凡人'],
